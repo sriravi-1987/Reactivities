9df6b643a420971def5f38194b99d0a9cb382f84
sriram ravi <sriram.ravi1002@gmail.com>
2025-09-17 21:33:48 +0530

    added cards

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,27 @@
+import { Box, Container, CssBaseline } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react"
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios.get<Activity[]>('https://localhost:7025/api/activities')
+      .then(response => setActivities(response.data))
+
+    return () => { }
+  }, []);
+  return (
+    <Box sx={{bgcolor: '#eeeeee'}}>
+      <CssBaseline />
+      <NavBar />
+      <Container maxWidth="xl" sx={{ mt: 3 }}>
+        <ActivityDashboard activities={activities}></ActivityDashboard>
+      </Container>
+    </Box>
+  )
+}
+
+export default App
---
+++ client/src/app/layout/NavBar.tsx
@@ -0,0 +1,33 @@
+import { Group } from '@mui/icons-material';
+import { Box, AppBar, Toolbar, Typography, Button, Container, MenuItem } from '@mui/material';
+
+export default function NavBar() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" sx={{ backgroundImage: 'linear-gradient(135dev, #182a73 0%, #218aae 69%, #20a7ac 89%)' }}>
+                <Container maxWidth="xl">
+                    <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
+                        <Box>
+                            <MenuItem sx={{display: 'flex', gap: 2}}>
+                                <Group fontSize='large' />
+                                <Typography variant="h4" fontWeight='bold' >Reactivities</Typography>
+                            </MenuItem>
+                        </Box>
+                        <Box sx={{display: 'flex'}}>
+                            <MenuItem sx={{fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold'}}>   
+                                Activities
+                            </MenuItem>
+                            <MenuItem sx={{fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold'}}>   
+                                About Us
+                            </MenuItem>
+                            <MenuItem sx={{fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold'}}>   
+                                Contact Us
+                            </MenuItem>
+                        </Box>
+                        <Button size='large' variant='contained' color='warning'>Create activity</Button>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </Box>
+    )
+}
